8ec216fd9eb7262f18302b01bae6690c64746bfa
Benjamin W. Portner <benjamin.portner@bauhaus-luftfahrt.net>
2021-09-17 11:34:21 +0200

    fix non-unicode character in datapackage.json which prevents parsing

--- mrio_common_metadata/conversion/exiobase_3_hybrid_io/datapackage.py
+++ mrio_common_metadata/conversion/exiobase_3_hybrid_io/datapackage.py
@@ -13,7 +13,7 @@ DATAPACKAGE = {
 References:
 
 * Merciai, Stefano, and Jannick Schmidt. 2016. [Physical/Hybrid Supply and Use Tables. Methodological Report. EU FP7 DESIRE Project](http://fp7desire.eu/documents/category/3-public-deliverables).
-* Merciai, Stefano, and Jannick Schmidt. 2018. [Methodology for the Construction of Global Multi-Regional Hybrid Supply and Use Tables for the EXIOBASE v3 Database](https://onlinelibrary.wiley.com/doi/full/10.1111/jiec.12713). Journal of Industrial Ecology 22, no. 3 (2018): 516–31. doi:10.1111/jiec.12713.""",
+* Merciai, Stefano, and Jannick Schmidt. 2018. [Methodology for the Construction of Global Multi-Regional Hybrid Supply and Use Tables for the EXIOBASE v3 Database](https://onlinelibrary.wiley.com/doi/full/10.1111/jiec.12713). Journal of Industrial Ecology 22, no. 3 (2018): 516-31. doi:10.1111/jiec.12713.""",
     "version": "3.3.17",
     "sources": [
         {
