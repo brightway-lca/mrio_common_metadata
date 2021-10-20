7a9ef202baf2b43e0fee56319cfadb856d745b64
Benjamin W. Portner <benjamin.portner@bauhaus-luftfahrt.net>
2021-10-20 12:10:54 +0200

    added typing for exio HIOT utils

--- mrio_common_metadata/conversion/exiobase_3_hybrid_io/utils.py
+++ mrio_common_metadata/conversion/exiobase_3_hybrid_io/utils.py
@@ -1,8 +1,10 @@
 import hashlib
 import pandas as pd
+from typing import Union
+from pathlib import Path
 
 
-def md5(filepath, blocksize=65536):
+def md5(filepath: Union[str, Path], blocksize: int = 65536):
     """Generate MD5 hash for file at `filepath`"""
     hasher = hashlib.md5()
     fo = open(filepath, "rb")
@@ -12,9 +14,10 @@ def md5(filepath, blocksize=65536):
         buf = fo.read(blocksize)
     return hasher.hexdigest()
 
-def append_to_index(df, columns):
+
+def append_to_index(df: pd.DataFrame, columns: dict) -> pd.DataFrame:
     index = df.index.to_frame(index=False)
-    for k,v in columns.items():
+    for k, v in columns.items():
         index[k] = v
     df.index = pd.MultiIndex.from_frame(index)
     return df
